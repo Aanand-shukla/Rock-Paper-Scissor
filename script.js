4f53e07272bb0a92abcb83b55ff3be733c0a71a8
Anand_Shukla <anandshuklagii@gmail.com>
2024-01-30 22:11:25 +0530

    some changes

--- script.js
+++ script.js
@@ -1,45 +1,45 @@
 const cpuMatch = document.querySelector(".cpuMatch img");
 const selectImages = document.querySelectorAll(".selectImages div");
 let userMatch = document.querySelector(".userMatch img");
-
+let result = document.querySelector(".result");
 let CpuImages = [
   "./images/Rock.png",
-  "./images/Scisscor.png",
   "./images/paper.png",
+  "./images/Scisscor.png",
 ];
+console.log(selectImages);
 
 selectImages.forEach((image, index) => {
-  let randomNumber = Math.trunc(Math.random() * 3);
-  image.addEventListener(
-    "click",
-    (e) => {
-      e.stopPropagation();
-      image.classList.add("active");
-      selectImages.forEach((image2, index2) => {
-        index !== index2 && image2.classList.remove("active");
-      });
-      let imgSrc = e.target.src;
-      userMatch.src = imgSrc;
-      cpuMatch.src = CpuImages[randomNumber];
-      let cpuValue = ["R", "P", "S"][randomNumber];
-      let userValue = ["R", "P", "S"][index];
-      let outcomes = {
-        RR: "Draw",
-        RP: "CPU",
-        RS: "User",
-        PP: "Draw",
-        PR: "CPU",
-        PS: "User",
-        SS: "Draw",
-        SR: "CPU",
-        SP: "User",
-      };
+  image.addEventListener("click", (e) => {
+    let randomNumber = Math.trunc(Math.random() * 3);
+    image.classList.add("active");
+    selectImages.forEach((image2, index2) => {
+      index !== index2 && image2.classList.remove("active");
+    });
+    let imgSrc = e.target.src;
+    userMatch.src = imgSrc;
+    cpuMatch.src = CpuImages[randomNumber];
 
-      let outComesValue = outcomes[userValue + cpuValue];
-      console.log(userValue + cpuValue);
+    let cpuValue = ["R", "P", "S"][randomNumber];
+    let userValue = ["R", "P", "S"][index];
 
-      console.log(outComesValue);
-    },
-    true
-  );
+    let outcomes = {
+      RR: "Draw",
+      RP: "CPU",
+      RS: "You",
+      PP: "Draw",
+      PR: "You",
+      PS: "CPU",
+      SS: "Draw",
+      SR: "CPU",
+      SP: "You",
+    };
+
+    let outComesValue = outcomes[userValue + cpuValue];
+
+    result.innerText =
+      cpuValue === userValue
+        ? `Match ${outComesValue}....`
+        : `${outComesValue} Win.....`;
+  });
 });
